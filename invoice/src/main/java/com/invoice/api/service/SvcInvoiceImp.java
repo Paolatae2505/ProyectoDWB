fa23b7243ca031dc096e84d0075c6a0cbf5fb7c2
Victoria Baron <victoriaebaronh@ciencias.unam.mx>
2023-05-25 20:38:07 -0600

    Avance pto 5

--- invoice/src/main/java/com/invoice/api/service/SvcInvoiceImp.java
+++ invoice/src/main/java/com/invoice/api/service/SvcInvoiceImp.java
@@ -1,5 +1,6 @@
 package com.invoice.api.service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,13 +26,13 @@ public class SvcInvoiceImp implements SvcInvoice {
 
 	@Autowired
 	RepoInvoice repo;
-	
+
 	@Autowired
 	RepoItem repoItem;
-	
+
 	@Autowired
 	RepoCart repoCart;
-	
+
 	@Autowired
 	ProductClient productCtl;
 
@@ -49,36 +50,62 @@ public class SvcInvoiceImp implements SvcInvoice {
 	public ApiResponse generateInvoice(String rfc) {
 		/*
 		 * Requerimiento 5
-		 * Implementar el método para generar una factura 
+		 * Implementar el método para generar una factura
 		 */
 		List<Cart> cartItems = repoCart.findByRfcAndStatus(rfc, 1);
 
-		if(cartItems.isEmpty()){
+		if (cartItems.isEmpty()) {
 			throw new ApiException(HttpStatus.NOT_FOUND, "the cart has no items");
 		}
-			
+
 		Map<Cart, DtoProduct> data = new HashMap<>();
+		Double total = 0.0;
+		Double taxes = 0.0;
+		Double subtotal = 0.0;
+		LocalDateTime created_at = LocalDateTime.now();
+
 		for (Cart cart : cartItems) {
-			data.put(cart, getDtoProduct(cart));
+			DtoProduct dtoProduct = getDtoProduct(cart);
+			Double unitPrice = dtoProduct.getPrice();
+			Integer quantity = cart.getQuantity();
+			Double productTotal = unitPrice * quantity;
+			Double productTaxes = productTotal * 0.16;
+			Double productSubtotal = productTotal - productTaxes;
+
+			total += productTotal;
+			taxes += productTaxes;
+			subtotal += productSubtotal;
+
+			// Actualizar stock 
+			productCtl.updateProductStock(dtoProduct.getGtin(), quantity);
+
+			data.put(cart, dtoProduct);
 		}
 
+		// Crear factura
+		Invoice invoice = new Invoice();
+		invoice.setRfc(rfc);
+		invoice.setSubtotal(subtotal);
+		invoice.setTaxes(taxes);
+		invoice.setTotal(total);
+		invoice.setCreated_at(created_at);
+		invoice.setStatus(1);
+
+		repo.save(invoice);
 		repoCart.clearCart(rfc);
-		
 		return new ApiResponse("invoice generated");
 	}
 
-	
-	
-	private DtoProduct getDtoProduct(Cart cart){
-		try{
+	private DtoProduct getDtoProduct(Cart cart) {
+		try {
 			ResponseEntity<DtoProduct> response = productCtl.getProduct(cart.getGtin());
-			if (response.getStatusCode() == HttpStatus.OK){
-				return  (DtoProduct) response.getBody();
+			if (response.getStatusCode() == HttpStatus.OK) {
+				return (DtoProduct) response.getBody();
 			} else {
 				String message = String.format("product %s does not exist", cart.getGtin());
 				throw new ApiException(HttpStatus.NOT_FOUND, message);
 			}
-		} catch(Exception e){
+		} catch (Exception e) {
 			throw new ApiException(HttpStatus.BAD_REQUEST, "unable to retrieve product information");
 		}
 	}
